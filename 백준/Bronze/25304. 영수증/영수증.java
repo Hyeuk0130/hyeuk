a64a9d720e441df6546d38a2fbf73e00abcb4969
Hyeuk0130 <gusgpdnr@naver.com>
2025-09-30 10:23:16 +0900

    [Bronze IV] Title: 영수증, Time: 188 ms, Memory: 17936 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int N = sc.nextInt();
+        int sum=0;
+        for(int i=1; i<=N; i++){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            sum+=A*B;
+        }
+        if (X==sum){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
