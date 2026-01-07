72291710f4b9bc10ed73567b9768bca530ddb9d9
Hyeuk0130 <gusgpdnr@naver.com>
2026-01-07 13:45:05 +0900

    [Bronze II] Title: 단어의 개수, Time: 612 ms, Memory: 63140 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine().trim();
+        if(S.isEmpty()){
+            System.out.println(0);
+        }
+        else{
+            String[] words = S.split(" +");
+            System.out.println(words.length);
+        }
+    }
+}
