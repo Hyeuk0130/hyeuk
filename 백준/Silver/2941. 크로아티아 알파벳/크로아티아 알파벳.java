3d4e8332a3882c2f002c4197cb0f621b2a85b2cd
Hyeuk0130 <gusgpdnr@naver.com>
2026-01-15 17:14:06 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 172 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        String[] croatia = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        for(String s : croatia){
+            word = word.replace(s,"*");
+        }
+        System.out.println(word.length());
+    }
+}
